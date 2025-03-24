4d634a0fa144606e936a01a4e9a470039e93425f
Martiniano-Bluealba <martiniano.leguizamon@bluealba.com>
2025-03-24 19:10:59 -0300

    Move Subscription Form Styling to its own file

--- src/components/SubscriptionForm/SubscriptionForm.jsx
+++ src/components/SubscriptionForm/SubscriptionForm.jsx
@@ -1,8 +1,16 @@
 import Card from "../common/Card/Card";
 import Button from "../common/Button/Button";
-import styled from "styled-components";
 import { Title, Text } from "../common/Texts/Texts.styled";
 import Input from "../common/Input/GeneralInput";
+import {
+  Form,
+  Image,
+  Body,
+  Information,
+  List,
+  ListItem,
+  Interactions,
+} from "./SubscriptionForm.styled";
 
 const SubscriptionForm = ({ onSubscribe }) => {
   return (
@@ -40,113 +48,3 @@ const SubscriptionForm = ({ onSubscribe }) => {
 };
 
 export default SubscriptionForm;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  gap: 1rem;
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-    min-height: unset;
-    gap: 2.5rem;
-    min-width: clamp(25rem, 80vw, 33rem);
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-    flex-direction: row-reverse;
-    gap: 4rem;
-    align-items: center;
-    max-width: 52.5rem;
-  }
-`;
-
-const Image = styled.div`
-  width: calc(100% + 48px);
-  height: auto;
-  transform: translate(-24px, -24px);
-  background-image: url("/src/assets/images/illustration-sign-up-mobile.svg");
-  background-size: cover;
-  background-position: center;
-  border-end-end-radius: 16px;
-  aspect-ratio: 1.32;
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-    width: 100%;
-    transform: unset;
-    border-radius: 16px;
-    height: 22.375rem;
-    background-position: unset;
-    aspect-ratio: unset;
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-    background-image: url("/src/assets/images/illustration-sign-up-desktop.svg");
-    height: 37.0625rem;
-    aspect-ratio: 0.67;
-  }
-`;
-
-const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 2.5rem;
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-    max-width: 38rem;
-    gap: 1.5rem;
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-    max-width: 56.5rem;
-    min-height: unset;
-    gap: 2rem;
-    min-width: 23.5rem;
-  }
-`;
-
-const Information = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-    gap: 2rem;
-  }
-`;
-
-const List = styled.ul`
-  padding-left: 0;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem; /* Adjust spacing between items */
-`;
-
-const ListItem = styled.li`
-  list-style: none;
-  display: grid;
-  grid-template-columns: auto 1fr;
-  gap: 1rem; /* Space between bullet and text */
-  align-items: center;
-
-  &::before {
-    content: "";
-    display: inline-block;
-    width: 21px;
-    height: 21px;
-    background-image: url("/src/assets/images/icon-success.svg");
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-`;
-
-const Interactions = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-    gap: 1rem;
-  }
-  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-    gap: 1.5rem;
-  }
-`;
---
+++ src/components/SubscriptionForm/SubscriptionForm.styled.jsx
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  gap: 1rem;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    min-height: unset;
+    gap: 2.5rem;
+    min-width: clamp(25rem, 80vw, 33rem);
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    flex-direction: row-reverse;
+    gap: 4rem;
+    align-items: center;
+    max-width: 52.5rem;
+  }
+`;
+
+export const Image = styled.div`
+  width: calc(100% + 48px);
+  height: auto;
+  transform: translate(-24px, -24px);
+  background-image: url("/src/assets/images/illustration-sign-up-mobile.svg");
+  background-size: cover;
+  background-position: center;
+  border-end-end-radius: 16px;
+  aspect-ratio: 1.32;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    width: 100%;
+    transform: unset;
+    border-radius: 16px;
+    height: 22.375rem;
+    background-position: unset;
+    aspect-ratio: unset;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    background-image: url("/src/assets/images/illustration-sign-up-desktop.svg");
+    height: 37.0625rem;
+    aspect-ratio: 0.67;
+  }
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    max-width: 38rem;
+    gap: 1.5rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    max-width: 56.5rem;
+    min-height: unset;
+    gap: 2rem;
+    min-width: 23.5rem;
+  }
+`;
+
+export const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    gap: 2rem;
+  }
+`;
+
+export const List = styled.ul`
+  padding-left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem; /* Adjust spacing between items */
+`;
+
+export const ListItem = styled.li`
+  list-style: none;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 1rem; /* Space between bullet and text */
+  align-items: center;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 21px;
+    height: 21px;
+    background-image: url("/src/assets/images/icon-success.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+`;
+
+export const Interactions = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    gap: 1rem;
+  }
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    gap: 1.5rem;
+  }
+`;
