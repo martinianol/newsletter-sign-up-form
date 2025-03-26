b0a4e335e4860aa096ec88a971572105788bc2ed
Martiniano-Bluealba <martiniano.leguizamon@bluealba.com>
2025-03-26 14:38:52 -0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/newsletter-sign-up-form/',  // Replace this with your repo name
   plugins: [react()],
 })
